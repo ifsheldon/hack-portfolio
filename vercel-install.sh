6747e6f56e921caf792a46db09e77dab13545f49
maple@max <feng.liang@kaust.edu.sa>
2025-04-29 20:38:55 +0800

    update script

--- vercel-install.sh
+++ vercel-install.sh
@@ -7,9 +7,5 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable --profil
 # Add cargo and rustup to PATH for this session
 export PATH="$HOME/.cargo/bin:$PATH"
 
-# Add the musl target for static linking
-rustup target add x86_64-unknown-linux-musl
-
-# Install dioxus-cli for the musl target
-cargo install dioxus-cli --target x86_64-unknown-linux-musl
+cargo install dioxus-cli
 
