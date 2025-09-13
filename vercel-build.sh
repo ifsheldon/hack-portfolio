625d51fd7903d076fa6cb53a351abad8dcc07a6f
ifsheldon <feng.liang@kaust.edu.sa>
2025-09-13 22:06:46 +0800

    use ssg in vercel build

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,7 +3,7 @@
 # This is the build script for vercel deployment
 
 export PATH="$HOME/.cargo/bin:$PATH"
-./dx bundle --platform web
+./dx bundle --platform web --ssg
 
 # remember to change "hack-portfolio" to your project name
 mv target/dx/hack-portfolio/release/web/public .
